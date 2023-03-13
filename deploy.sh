f48b2bfbb60cf9ea44888bc82fab80e9bdc7ecc2
Alexander Parco <alexparco16@gmail.com>
2023-03-13 14:30:23 -0500

    resolve build

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:alexparco/alexparco.github.io.git main
+# git push -f git@github.com:alexparco/alexparco.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:alexparco/portfolio.git main:gh-pages
+git push -f git@github.com:alexparco/portfolio.git main:gh-pages
 
 cd -
