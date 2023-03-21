2f4625b179b3cb7b6b2f5291c6105def5b30a207
Alexander Parco <alexparco16@gmail.com>
2023-03-21 12:36:14 -0500

    chage grade

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -6,7 +6,7 @@ const Profile = () => {
       mt={8}
     >
       <Text fontSize='17'>
-        Hey! Soy un estudiante de 4.º ciclo de la carrera de Diseño y Desarrollo de Software en TECSUP. Cuento con una gran capacidad de solución de problemas, autodesarrollo, comunicación efectiva y adaptabilidad. Mi interes esta centrado en desarrollar aplicaciones con el stack <strong>ReactJs y Java</strong>
+        Hey! Soy un estudiante de 5.º ciclo de la carrera de Diseño y Desarrollo de Software en TECSUP. Cuento con una gran capacidad de solución de problemas, autodesarrollo, comunicación efectiva y adaptabilidad. Mi interes esta centrado en desarrollar aplicaciones con el stack <strong>ReactJs y Java</strong>
       </Text>
     </Box >
   )
