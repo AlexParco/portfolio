93054ac7ab4c635b5a52e74fe6e252529cad9896
Alexander Parco <alexparco16@gmail.com>
2023-03-21 15:54:52 -0500

    stack javascript

--- src/data/stack.ts
+++ src/data/stack.ts
@@ -8,6 +8,9 @@ export const StackData: Stack[] = [
     alt: 'TypeScript',
     src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg'
   }, {
+    alt: 'JavaScript',
+    src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg'
+  }, {
     alt: 'Git',
     src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg'
   }, {
