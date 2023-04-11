18a81f4c153a4ff8c7ea8011f7a3db6f07a67e46
AlexParco <alexparco16@gmail.com>
2023-04-10 22:28:21 -0500

    docs: change profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,10 +3,10 @@ import { Box, Text } from "@chakra-ui/react"
 const Profile = () => {
   return (
     <Box
-      mt={8}
+      mt={4}
     >
       <Text fontSize='17'>
-        Hey! Soy un estudiante de 5.º ciclo de la carrera de Diseño y Desarrollo de Software en TECSUP. Cuento con una gran capacidad de solución de problemas, autodesarrollo, comunicación efectiva y adaptabilidad. Mi interes esta centrado en desarrollar aplicaciones con el stack <strong>ReactJs y Java</strong>
+        Soy un desarrollador web con más de 2 años de experiencia en programación. Mi enfoque principal es desarrollar software altamente escalable mediante la aplicación de metodologías ágiles. Además, tengo habilidades en la resolución de problemas, la gestión de proyectos y la capacidad de trabajar en equipo para asegurar la entrega exitosa de cada proyecto.
       </Text>
     </Box >
   )
